9ed9308c34b228cfb8ab66341698f3b9bf21cebe
bedawang <christof.becu@gmail.com>
2019-06-17 11:51:14 +0200

    Look if specific key exists in hash

--- data-types/collections/hashes.rb
+++ data-types/collections/hashes.rb
@@ -243,7 +243,7 @@ class HashesTest < Minitest::Test
   end
 
   def test_19
-    skip
+
     ages = {
       Jimmy: 4,
       Julio: 8,
@@ -251,12 +251,12 @@ class HashesTest < Minitest::Test
     }
     # Call a method on the ages hash defined above
     # to figure out if :Jimmy is a key
-    jimmy_in_hash = ______
+    jimmy_in_hash = ages.key?(:Jimmy)
     assert_equal true, jimmy_in_hash
 
     # Now figure out if :Jackie is in the hash
 
-    jackie_in_hash = ______
+    jackie_in_hash = ages.key?(:jackie)
     assert_equal false, jackie_in_hash
   end
 
