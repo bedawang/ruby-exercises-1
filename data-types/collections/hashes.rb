8f2d61fcdfccdb9ca934531238e9eb1bdbd25c8d
bedawang <christof.becu@gmail.com>
2019-06-17 11:28:21 +0200

    Create hash with default value of 0

--- data-types/collections/hashes.rb
+++ data-types/collections/hashes.rb
@@ -45,12 +45,13 @@ class HashesTest < Minitest::Test
   def test_5
     # In the line below, create a new hash with
     # a default value of zero
-    ingredients = ______
+    ingredients = Hash.new(0)
     assert_equal 0, ingredients["tomatoes"]
     assert_equal 0, ingredients[:carrots]
   end
 
   def test_6
+    skip
     books = {
       "John Steinbeck" => "Grapes of Wrath",
       "Harper Lee" => "To Kill a Mockingbird"
@@ -62,6 +63,7 @@ class HashesTest < Minitest::Test
   end
 
   def test_7
+    skip
     books = {
       "John Steinbeck" => "Grapes of Wrath",
       "Harper Lee" => "To Kill a Mockingbird"
@@ -79,7 +81,9 @@ class HashesTest < Minitest::Test
     assert_equal expected, books
   end
 
+
   def test_8
+    skip
     books = {
       "John Steinbeck" => "Grapes of Wrath",
       "Harper Lee" => "To Kill a Mockingbird"
@@ -96,6 +100,7 @@ class HashesTest < Minitest::Test
   end
 
   def test_9
+    skip
     books = {
       "John Steinbeck" => "Grapes of Wrath",
       "Harper Lee" => "To Kill a Mockingbird"
@@ -110,6 +115,7 @@ class HashesTest < Minitest::Test
   end
 
   def test_10
+    skip
     books = {
       John_Steinbeck: "Grapes of Wrath",
       Harper_Lee: "To Kill a Mockingbird"
@@ -121,6 +127,7 @@ class HashesTest < Minitest::Test
   end
 
   def test_11
+    skip
     books = {
       John_Steinbeck: "Grapes of Wrath",
       Harper_Lee: "To Kill a Mockingbird"
@@ -139,6 +146,7 @@ class HashesTest < Minitest::Test
   end
 
   def test_12
+    skip
     books = {
       John_Steinbeck: "Grapes of Wrath",
       Harper_Lee: "To Kill a Mockingbird"
@@ -155,6 +163,7 @@ class HashesTest < Minitest::Test
   end
 
   def test_13
+    skip
     books = {
       John_Steinbeck: "Grapes of Wrath",
       Harper_Lee: "To Kill a Mockingbird"
@@ -169,6 +178,7 @@ class HashesTest < Minitest::Test
   end
 
   def test_14
+    skip
     ages = {
       "Jimmy" => 4,
       "Julio" => 8,
@@ -181,6 +191,7 @@ class HashesTest < Minitest::Test
   end
 
   def test_15
+    skip
     ages = {
       Jimmy: 4,
       Julio: 8,
@@ -193,6 +204,7 @@ class HashesTest < Minitest::Test
   end
 
   def test_16
+    skip
     ages = {
       Jimmy: 4,
       Julio: 8,
@@ -205,6 +217,7 @@ class HashesTest < Minitest::Test
   end
 
   def test_17
+    skip
     ages = {
       Jimmy: 4,
       Julio: 8,
@@ -217,6 +230,7 @@ class HashesTest < Minitest::Test
   end
 
   def test_18
+    skip
     ages = {
       Jimmy: 4,
       Julio: 8,
@@ -229,6 +243,7 @@ class HashesTest < Minitest::Test
   end
 
   def test_19
+    skip
     ages = {
       Jimmy: 4,
       Julio: 8,
@@ -246,6 +261,7 @@ class HashesTest < Minitest::Test
   end
 
   def test_20
+    skip
     ages = {
       Jimmy: 4,
       Julio: 8,
